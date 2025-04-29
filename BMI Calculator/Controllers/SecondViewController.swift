244a5e46f177c4d06de07df0944a865f7ecee30e
PengiunXC <113483621+PenguinXC@users.noreply.github.com>
2025-04-29 18:03:59 +0700

    124. How to Use the Docs in Xcode and Create a Custom UIViewController Class

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by VuNA on 29/4/25.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
