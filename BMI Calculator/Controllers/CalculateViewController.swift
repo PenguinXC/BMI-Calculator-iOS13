9392c5a93254c4d68b6381bbced510adcd484c59
PengiunXC <113483621+PenguinXC@users.noreply.github.com>
2025-04-30 18:06:19 +0700

    129. Use Optionals in Practice and Add the Finishing Touches with Colour Literals

--- BMI Calculator/Controllers/CalculateViewController.swift
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -47,6 +47,8 @@ class CalculateViewController: UIViewController {
             let destinationVC = segue.destination as! ResultsViewController
             
             destinationVC.bmiValue = calculatorBrain.getBMIValue()
+            destinationVC.advice = calculatorBrain.getAdvice()
+            destinationVC.color = calculatorBrain.getColor()
         }
     }
         
---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by VuNA on 30/4/25.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
